4bf29bc86c6ff0d4478f0a71564a40735e7fa8c0
Zibnu <crimhomid@gmail.com>
2026-01-25 12:27:36 +0700

    connect to postgresDB

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-const express = require("express");
+    const express = require("express");
+    const app = express();
+    const path = require("path");
+    require("dotenv").config();
+
+
+    const sequelize = require("./src/config/db");
+
+    app.use(express.json());
+    app.use(express.urlencoded({extended : true}));
+
+    // (async () => {
+    //     try {
+    //         await sequelize.authenticate();
+    //         console.log("Connected SUCCESS");
+    //     } catch (error) {
+    //         console.error("Failed", error);
+    //     }
+    // })();
+
+
+    const PORT = process.env.PORT
+    app.listen(PORT ,() => console.log("Server is Running"));
