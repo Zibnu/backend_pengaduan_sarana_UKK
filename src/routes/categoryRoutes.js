7b364132c8c3161b2e4f3ed7808eb3399af96909
Zibnu <crimhomid@gmail.com>
2026-02-07 23:21:37 +0700

    access publik for data room and category

--- src/routes/categoryRoutes.js
+++ src/routes/categoryRoutes.js
@@ -4,8 +4,8 @@ const categoryController = require("../controllers/categoryController");
 const { authenticate , isAdmin } = require("../middleware/authMiddleware");
 
 router.post("/create_category", authenticate, isAdmin, categoryController.createCategory);
-router.get("/get_all_category", authenticate, categoryController.getAllCategories);
-router.get("/get_category/:id", authenticate, categoryController.getCategoryById);
+router.get("/get_all_category", categoryController.getAllCategories);
+router.get("/get_category/:id", categoryController.getCategoryById);
 router.delete("/del_category/:id", authenticate, isAdmin, categoryController.deleteCategory);
 
 module.exports = router;
--- src/routes/roomRoutes.js
+++ src/routes/roomRoutes.js
@@ -4,8 +4,8 @@ const roomController = require("../controllers/roomController");
 const { authenticate, isAdmin } = require("../middleware/authMiddleware");
 
 router.post("/create_room", authenticate, isAdmin, roomController.createRoom);
-router.get("/get_all_room", authenticate, roomController.getAllRooms);
-router.get("/get_room/:id", authenticate, roomController.getRoomById);
+router.get("/get_all_room", roomController.getAllRooms);
+router.get("/get_room/:id", roomController.getRoomById);
 router.delete("/del_room/:id", authenticate, isAdmin, roomController.deleteRoom);
 
 module.exports = router;
